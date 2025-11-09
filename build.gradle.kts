1c048439bc52764d11ba921f745c41645fa53cf5
tomasvr-crypto <tomasvr@seznam.cz>
2025-11-09 14:30:28 +0100

    Create build.gradle.kts

---
+++ build.gradle.kts
@@ -0,0 +1,5 @@
+plugins {
+    kotlin("jvm") version "1.8.21" apply false
+    id("com.android.application") version "8.1.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.8.21" apply false
+}
